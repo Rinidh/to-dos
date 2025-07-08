c00b4d5c73d4162a83c681bb6efc8c78427a9677
Rinidh <rinidhdhokia@gmail.com>
2025-07-08 22:00:54 +0300

    Add a cross character at right end of every to-do that appears upon hover-in Create toDoHTML() to store html code for list-item (new to-dos)

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const toDos = storage ? JSON.parse(storage) : []
 input.addEventListener("change", function () { // don't use arrow functions if you are to use `this` inside the function block as it will lead to Window global object instead of the input
   if (!this.value) return
 
-  toDos.push({ value: this.value, isDone: false })
+  toDos.unshift({ value: this.value, isDone: false }) // use Array.unshift to add item (todo) at beginning of array, hence recently created to-dos will appear topmost
   localStorage.setItem("toDos", JSON.stringify(toDos))
 
-  const newToDo = `<li class="todos-item lato-bold"> <input type="checkbox" id="${this.value}"> <label htmlFor="${this.value}">${this.value}</label> </li>`
+  const newToDo = toDoHTML(this.value)
   toDosList.innerHTML = newToDo + toDosList.innerHTML
 
   this.value = ""
@@ -19,8 +19,18 @@ input.addEventListener("change", function () { // don't use arrow functions if y
 document.addEventListener("DOMContentLoaded", function () {
   if (toDos.length < 1) return;
 
-  const toDosHTML = toDos.map(toDo => `<li class="todos-item lato-bold"> <input type="checkbox" id="${toDo.value}"> <label htmlFor="${toDo.value}">${toDo.value}</li>`)
+  const toDosHTML = toDos.map(toDo => toDoHTML(toDo.value))
   toDosList.innerHTML = toDosHTML.join("")
 })
+
+function toDoHTML(text) {
+  return `<li class="todos-item lato-bold">
+            <span>
+              <input type="checkbox" id="${text}">
+              <label htmlFor="${text}">${text}</label>
+            </span>
+            <span class="cross">×</span>
+          </li>`
+}
 
 

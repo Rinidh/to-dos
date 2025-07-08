c61d1c21c5ae5b25331280dd3ca3e87bfc78582b
Rinidh <rinidhdhokia@gmail.com>
2025-07-08 21:29:01 +0300

    Design a custom checkmark that appears upon checking/clicking the to-do

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ const toDosList = document.querySelector(".todos-list")
 
 const storage = localStorage.getItem("toDos")
 const toDos = storage ? JSON.parse(storage) : []
-// DUMMY:
-// [
-//   {
-//     "value": "krishna",
-//     "isDone": false
-//   },
-//   {
-//     "value": "radhe",
-//     "isDone": false
-//   }
-// ]
 
 input.addEventListener("change", function () { // don't use arrow functions if you are to use `this` inside the function block as it will lead to Window global object instead of the input
   if (!this.value) return
